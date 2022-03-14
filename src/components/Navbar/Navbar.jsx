186bb21896c30bed0e78ed5603a935011495b86f
eliwhalen <39849361+AlwaysFocus@users.noreply.github.com>
2022-03-13 21:21:51 -0600

    Built header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { HiMenuAlt4, HiX } from "react-icons/hi";
 import { motion } from "framer-motion";
 
-import Images from "../../constants/images";
+import {Images} from "../../constants";
 
 import "./Navbar.scss";
 
--- src/constants/images.js
+++ src/constants/images.js
@@ -28,6 +28,7 @@ import profile from "../assets/profile.png";
 import circle from "../assets/circle.svg";
 import logo from "../assets/logo.png";
 import eliLogo from "../assets/EliLogo.png";
+import profilePic from "../assets/ProfilePic.png";
 
 import adidas from "../assets/adidas.png";
 import amazon from "../assets/amazon.png";
@@ -65,6 +66,7 @@ export default {
   circle,
   logo,
   eliLogo,
+  profilePic,
   adidas,
   amazon,
   asus,
