4d884b39e33f5fcfcbc9ef5267ca3994b32c3163
eliwhalen <39849361+AlwaysFocus@users.noreply.github.com>
2022-03-20 16:29:07 -0600

    Migrated about section content to Sanity.

---
+++ src/client.js
@@ -0,0 +1,18 @@
+import sanityClient from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+
+export const client = sanityClient({
+  // This is retrieved by going into the backend dir
+  // and running 'sanity manage'
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+
+  dataset: "production",
+  apiVersion: "2022-03-20",
+  useCdn: true,
+
+  token: process.env.REACT_APP_SANITY_TOKEN,
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
