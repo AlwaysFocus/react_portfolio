9178b018c51f9256cb21114de55cc04aa862a4db
eliwhalen <39849361+AlwaysFocus@users.noreply.github.com>
2022-03-27 20:59:11 -0600

    Updated header icons

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -60,7 +60,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[Images.flutter, Images.redux, Images.sass].map((circle, index) => (
+        {[Images.react, Images.javascript, Images.sass].map((circle, index) => (
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={circle} alt="circle" />
           </div>
